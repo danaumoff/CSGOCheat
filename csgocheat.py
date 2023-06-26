067320636f07a79b17e51b636d3b1b378a21029e
Naumov Dmitry <deemanaumov@gmail.com>
2023-06-26 22:13:05 +0300

    completed

---
+++ csgocheat.py
@@ -0,0 +1,35 @@
+import pymem
+import re
+import keyboard
+from colorama import init
+
+init()
+
+
+def on_triggered():
+    pm = pymem.Pymem('csgo.exe')
+    client = pymem.process.module_from_name(pm.process_handle, 'client.dll')
+
+    clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
+    address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F', clientModule).start() + 2
+    pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
+
+keyboard.add_hotkey('f9', on_triggered)
+
+print("\u001b[36;1m"+"""
+,--.   ,--.        ,--.,--.,--.  ,--.              ,--.     
+|  |   |  | ,--,--.|  ||  ||  '--'  | ,--,--. ,---.|  |,-.  
+|  |.'.|  |' ,-.  ||  ||  ||  .--.  |' ,-.  || .--'|     /  
+|   ,'.   |\ '-'  ||  ||  ||  |  |  |\ '-'  |\ `--.|  \  \  
+'--'   '--' `--`--'`--'`--'`--'  `--' `--`--' `---'`--'`--' 
+""")
+print("By HackTeam\n-Ikdan\n-DeadCode")
+print("Активация/Activation: F9")
+
+while True:
+    input()
+
+
+                                                            
+
+                                                            
